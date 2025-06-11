c190f0f0ec9a583e40f22576b722db21ed5da6b5
CirceHathaway <estebancarp72@gmail.com>
2025-06-11 20:39:27 -0300

    + devocionales

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'biblia-digital-v13'; // Cambiamos el nombre para forzar una nueva instalación
+const CACHE_NAME = 'biblia-digital-v14'; // Cambiamos el nombre para forzar una nueva instalación
 const urlsToCache = [
   '/',
   '/biblia.digital/index.html',

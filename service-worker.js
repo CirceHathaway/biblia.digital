22af215ae116b782c3d745c81e8901f358549e6e
CirceHathaway <estebancarp72@gmail.com>
2025-06-15 22:01:31 -0300

    Arreglo de nav.capitulo para celular

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'biblia-digital-v22'; // Cambiamos el nombre para forzar una nueva instalación
+const CACHE_NAME = 'biblia-digital-v23'; // Cambiamos el nombre para forzar una nueva instalación
 const urlsToCache = [
   '/',
   '/biblia.digital/index.html',

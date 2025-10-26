8df87fa4aecf098e0d94402ff74f9ceb0a1efc9c
CirceHathaway <estebancarp72@gmail.com>
2025-10-26 05:59:31 -0300

    arreglo del boton siguiente > y quito la radio del footer

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'mbiblia-v51';
+const CACHE_NAME = 'mbiblia-v52';
 const SCOPE = self.registration.scope; // p.ej. https://usuario.github.io/biblia.digital/
 
 // Utilidad: resuelve un path relativo al scope del SW
